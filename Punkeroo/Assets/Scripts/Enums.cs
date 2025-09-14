25b0b5929b5e893cda049bfcd2521c44814a3c8a
SoulexDev <fnafiplayar@gmail.com>
2025-09-14 02:30:59 -0700

    Update .vsconfig, Art.meta, Sprites.meta, and 5808 more files

--- Punkeroo/Assets/Scripts/Enums.cs
+++
@@ -1,4 +0,0 @@
-public enum PlayerState { Idle, Run, Jump, Fall, Dash }
-public enum DungeonRoomHorizontalState { HorizontalRight, HorizontalLeft, Both }
-public enum DungeonRoomVerticalState { VerticalUp, VerticalDown, Both }
-public enum Direction { Up = 0, Down = 1, Left = 2, Right = 3 }
